15bdd18bb249f9d55899fd38ab92bd04dc719c4c
forgiveboo <wyx5051@gmail.com>
2021-11-23 16:11:10 +0800

    update

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -155,14 +155,3 @@ require'lspconfig'.jdtls.setup{
 --    }, --or vim.fn.getcwd(),
   single_file_mode = true
 }
-
-vim.notify = function(msg, log_level, _opts)
-  if msg:match("exit code") then
-      return
-  end
-  if log_level == vim.log.levels.ERROR then
-    vim.api.nvim_err_writeln(msg)
-  else
-    vim.api.nvim_echo({{msg}}, true, {})
-  end
-end
