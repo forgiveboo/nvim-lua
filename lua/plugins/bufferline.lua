daefccc41aa06bdea55313f2e56d053d0d3a37a0
forgiveboo <wyx5051@gmail.com>
2021-11-24 00:27:23 +0800

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,7 @@
 require('bufferline').setup {
   options = {
-    numbers = "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    numbers = "buffer_id",
     --- @deprecated, please specify numbers as a function to customize the styling
-    number_style = "superscript" | "subscript" | "" | { "none", "subscript" }, -- buffer_id at index 1, ordinal at index 2
     close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
     right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
@@ -21,15 +20,15 @@ require('bufferline').setup {
     --- bufferline so use this at your discretion knowing that it has
     --- some limitations that will *NOT* be fixed.
     name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-      -- remove extension from markdown files for example
+     -- remove extension from markdown files for example
       if buf.name:match('%.md') then
-        return vim.fn.fnamemodify(buf.name, ':t:r')
+         return vim.fn.fnamemodify(buf.name, ':t:r')
       end
     end,
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 18,
-    diagnostics = false | "nvim_lsp" | "coc",
+    diagnostics = "nvim_lsp",
     diagnostics_update_in_insert = false,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       return "("..count..")"
@@ -50,20 +49,19 @@ require('bufferline').setup {
         return true
       end
     end,
-    offsets = {{filetype = "NvimTree", text = "File Explorer" | function , text_align = "left" | "center" | "right"}},
-    show_buffer_icons = true | false, -- disable filetype icons for buffers
-    show_buffer_close_icons = true | false,
-    show_close_icon = true | false,
-    show_tab_indicators = true | false,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    -- can also be a table containing 2 custom separators
-    -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "slant" | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = false | true,
-    always_show_bufferline = true | false,
-    sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-      -- add custom logic
-      return buffer_a.modified > buffer_b.modified
-    end
-  }
+    offsets = {{filetype = "NvimTree", text = "File Explorer",
+      show_buffer_icons = true, -- disable filetype icons for buffers
+      show_buffer_close_icons = true,
+      show_close_icon = true,
+      show_tab_indicators = true,
+      persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+      -- can also be a table containing 2 custom separators
+      -- [focused and unfocused]. eg: { '|', '|' }
+      separator_style = "slant",
+      enforce_regular_tabs = false,
+      always_show_bufferline = true,
+      sort_by = 'id'
+    },
+  }-- add custom logic
+}
 }
