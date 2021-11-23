cd12c56b5ee0e896e4cb1df1fb22ea7ea335a709
forgiveboo <wyx5051@gmail.com>
2021-11-23 21:54:03 +0800

    update

--- lua/lsp/jdtls.lua
+++ lua/lsp/jdtls.lua
@@ -1,6 +1,23 @@
 local nvim_lsp = require('lspconfig')
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		'documentation',
+		'detail',
+		'additionalTextEdits',
+	},
+}
+
 nvim_lsp.jdtls.setup {
   cmd = { 
     "/usr/lib/jvm/default/bin/java", 
--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,5 +1,22 @@
 local nvim_lsp = require('lspconfig')
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		'documentation',
+		'detail',
+		'additionalTextEdits',
+	},
+}
+
 local jedi_root_path = os.getenv('HOME') .. '/.local/share/nvim/lsp_servers/jedi_language_server'
 
 nvim_lsp.jedi_language_server.setup {
