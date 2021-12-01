2d078807fc5cf13f89b0a5c4dd118201a89285fa
forgiveboo <wyx5051@gmail.com>
2021-12-01 18:19:54 +0800

    update

--- lua/tools/keymaps.lua
+++ lua/tools/keymaps.lua
@@ -6,17 +6,17 @@ vim.g.maplocalleader = " "
 local map = vim.api.nvim_set_keymap
 local opt = {noremap = true, silent = true }
 
-map('n', '<c-p>', "<cmd>lua require('fzf-lua').files()<CR>", opt)
-map('n', '<c-n>', ":NvimTreeToggle<CR>", opt)
+map('n', '<A-p>', "<cmd>lua require('fzf-lua').files()<CR>", opt)
+map('n', '<A-n>', ":NvimTreeToggle<CR>", opt)
 map('n', '<A-i>', '<cmd>lua require("FTerm").toggle()<CR>', opt)
 map('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opt)
 map('n', '<A-c>', '<cmd>lua require("fine-cmdline").open()<CR>', opt)
 map('n', '<A-s>', '<Cmd>lua require(\'jdtls\').code_action()<CR>', opt)
 map('n', '<C-x>', ':q<CR>', opt)
-map('n', '<c-s>', ':w<CR>', opt)
+map('n', '<C-s>', ':w<CR>', opt)
 map('n', '<A-p>', ':Glow<CR>', opt)
 map('n', '<F5>',  ':SnipRun<CR>', opt)
 map('n', '<C-l>', ':BufferLineCycleNext<CR>',opt)
 map('n', '<C-h>', ':BufferLineCyclePrev<CR>', opt)
-map('n', '<C-j>', ':BufferLineMoveNext<CR>', opt)
-map('n', '<C-k>', ':BufferLineMovePrev<CR>', opt)
+map('n', '<C-p>', ':BufferLinePick<CR>', opt)
+map('n', '<C-d>', ':BufferLinePickClose<CR>', opt)
