4481177f86490a6a50114dc616c5a83139f9c4ba
forgiveboo <wyx5051@gmail.com>
2021-12-02 00:04:06 +0800

    update

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -8,6 +8,8 @@ vim.o.completeopt = "menuone,noselect"
 local luasnip = require "luasnip"
 local lspkind = require("lspkind")
 
+local compare = require('cmp.config.compare')
+
 local cmp = require'cmp'
 
 -- 自动提示1 详情信息
@@ -59,6 +61,19 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      compare.offset,
+      compare.exact,
+      compare.score,
+      compare.recently_used,
+      compare.kind,
+      compare.sort_text,
+      compare.length,
+      compare.order,
+    },
+  },
   mapping = {
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -72,18 +87,14 @@ cmp.setup({
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
     { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
     {name = 'nvim_lua'}
   }, {
     { name = 'buffer' },
     { name = 'cmdline'},
     { name = 'path' }
-  })
+  }), 
 })
-
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
   sources = {
