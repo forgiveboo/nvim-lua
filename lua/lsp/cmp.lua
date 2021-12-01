5e92ab8921aa844d7a59d0cee18d03d068a72297
forgiveboo <wyx5051@gmail.com>
2021-12-01 20:40:33 +0800

    update

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -56,10 +56,7 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
     end,
   },
   mapping = {
@@ -102,7 +99,3 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
-
---require("lsp.lua")
---require('lsp.python')
---require('lsp.jdtls')
