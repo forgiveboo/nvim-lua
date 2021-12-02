59f02b7a5ae3c73306d7d7bdfbe65f833321059a
forgiveboo <wyx5051@gmail.com>
2021-12-02 19:58:09 +0800

    update

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,1 +1,20 @@
-vim.g.dashboard_default_executive ='fzf'
+vim.g.dashboard_default_executive ='telescope'
+vim.g.dashboard_custom_shortcut = {
+  last_session       = ' SPC sl ',
+  find_history       = ' SPC fh ',
+  find_file          = ' SPC ff ',
+  new_file           = ' SPC cn ',
+  change_colorscheme = ' SPC tc ',
+  find_word          = ' SPC fa ',
+  book_marks         = ' SPC fb '
+}
+vim.g.dashboard_custom_header = {
+  "                                                     ",
+  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+  "                                                     ",
+}
